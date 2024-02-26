06022230f0e88c11a4476de057a2ed86cabf6c71
aleeshyakrishna <aleeshyakrishnamv@gmail.com>
2024-02-26 12:40:04 +0530

    baseUrl changed

--- src/utils/baseUrl.jsx
+++ src/utils/baseUrl.jsx
@@ -2,7 +2,8 @@ import axios  from "axios"
 
 const api = axios.create({
     // baseURL : "https://storyclub-backend.onrender.com"
-    baseURL : "http://localhost:3000"
+    // baseURL : "http://localhost:3000"
+    baseURL:"https://storyclub-backend-1.onrender.com"
 })
 
 api.interceptors.request.use(
